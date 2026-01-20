3f02d8454c1dbbbf5244c31aa729b056f7fa1214
Haritha sabbani <sabbaniharitha2203@gmail.com>
2026-01-20 22:51:09 +0530

    “assignment

--- typescript-training/assignments/assignment6.ts
+++ typescript-training/assignments/assignment6.ts
@@ -2,7 +2,12 @@ function isprime(num:number):boolean{
     if(num<=1){
         return false;
     }
-    for(let i:number=2;i<=Math.sqrt(num);i++){
+    if(num===2){
+        return false;
+    }
+
+    const sqrt=Math.sqrt(num);
+    for(let i:number=3;i<=Math.sqrt(num);i+=2){
         if(num%i===0){
             return false;
         }
@@ -10,7 +15,7 @@ function isprime(num:number):boolean{
     return true;
 }
 
-let numberToCheck:number=29;
+let numberToCheck:number=2;
 if(isprime(numberToCheck)){
     console.log(`${numberToCheck} is a prime number.`);
 }else{
